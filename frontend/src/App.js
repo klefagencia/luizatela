c776c9cc1a152271a89a3cdcd3c79545777ddbbe
klefagencia <klefagencia@gmail.com>
2026-07-07 11:34:25 -0300

    feat: proteger rota /calculadora com CalculatorGate (login)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,22 @@ import { Toaster } from "sonner";
 import LandingPage from "@/pages/LandingPage";
 import CalculatorPage from "@/pages/CalculatorPage";
 import { CalculatorProvider } from "@/contexts/CalculatorContext";
+import CalculatorGate from "@/components/CalculatorGate";
 
 function App() {
   return (
     <div className="App noise">
-      <CalculatorProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/calculadora" element={<CalculatorPage />} />
-          </Routes>
-        </BrowserRouter>
-        <Toaster theme="dark" position="top-right" richColors />
-      </CalculatorProvider>
-    </div>
-  );
+    <CalculatorProvider>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<LandingPage />} />
+<Route path="/calculadora" element={<CalculatorGate><CalculatorPage /></CalculatorGate>} />
+  </Routes>
+  </BrowserRouter>
+<Toaster theme="dark" position="top-right" richColors />
+  </CalculatorProvider>
+  </div>
+);
 }
 
 export default App;
